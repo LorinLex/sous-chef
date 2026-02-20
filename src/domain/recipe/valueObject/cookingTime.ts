60f618b3305351d90126badd6dfca31044c1d13a
Emil Mukhitdinov <mukhitdinov.er@sirius.online>
2026-02-20 22:41:58 +0300

    Add CookingTime valueObject

---
+++ src/domain/recipe/valueObject/cookingTime.ts
@@ -0,0 +1,32 @@
+interface CookingTimeProps{
+    prepareTime: number
+    cookingTime: number
+}
+
+class CookingTime {
+    private constructor (private props: CookingTimeProps) {}
+
+    public create(props: CookingTimeProps) {
+        if (props.prepareTime < 0) throw new Error("Prepare time must be non negative!")
+        if (props.cookingTime < 0) throw new Error("Cooking time must be non negative!")
+        if (props.prepareTime + props.cookingTime <= 0) throw new Error("Recipe time must be greater than zero!")
+
+        return new CookingTime(props)
+    }
+
+    public rehydrate(props: CookingTimeProps) {
+        return new CookingTime(props)
+    }
+
+    get prepareTime() {
+        return this.props.prepareTime
+    }
+
+    get cookingTime() {
+        return this.props.cookingTime
+    }
+
+    get fullTime() {
+        return this.props.prepareTime + this.props.cookingTime
+    }
+}
