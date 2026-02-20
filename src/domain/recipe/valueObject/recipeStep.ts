fd6288294ba504412503bd641f7a69df5d8c12f2
Emil Mukhitdinov <mukhitdinov.er@sirius.online>
2026-02-20 22:34:34 +0300

    Add recipe step valueObject

---
+++ src/domain/recipe/valueObject/recipeStep.ts
@@ -0,0 +1,16 @@
+interface RecipeStepProps {
+    order: number
+    description: string
+}
+
+export class RecipeStep {
+    private constructor (private props: RecipeStepProps) {}
+
+    public static create(props: RecipeStepProps): RecipeStep {
+        if (props.order < 1) throw new Error("Invalid step order");
+        if (!props.description.trim()) throw new Error("Description required");
+
+        return new RecipeStep(props)
+    }
+
+}
